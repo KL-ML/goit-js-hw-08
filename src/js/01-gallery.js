198bc946d5deb7f379561a3c1b58fb14845a9449
Natalya Karpova <knataly0513@gmail.com>
2022-10-06 20:35:13 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const makeOneImageMarkup = ({ preview, original, description }) => {
+    return `
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}">
+        </a>
+    `;
+};
+const makeGaleryMarkup = galleryItems
+    .map(makeOneImageMarkup)
+    .join('');
+gallery.insertAdjacentHTML('beforeend', makeGaleryMarkup);
+
+new SimpleLightbox('div.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
